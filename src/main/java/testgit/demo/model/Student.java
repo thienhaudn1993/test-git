442b15235de8b5e9a53668679ac21d648d4a292c
thienhaudn1993 <87653095+thienhaudn1993@users.noreply.github.com>
2022-05-30 00:25:39 +0700

    tao repository cho student-A

--- src/main/java/testgit/demo/model/Student.java
+++ src/main/java/testgit/demo/model/Student.java
@@ -1,22 +1,22 @@
-package model;
+package testgit.demo.model;
 
 public class Student {
-    int id;
-    String name;
+    private Long id;
+    private String name;
 
     public Student() {
     }
 
-    public Student(int id, String name) {
+    public Student(Long id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
