6247404ea83506fab3eb570977fa62d4297958cc
thienhaudn1993 <87653095+thienhaudn1993@users.noreply.github.com>
2022-05-30 10:41:10 +0700

    HauLST - update getall ben service

---
+++ src/main/java/service/Impl/TeacherBSerivce.java
@@ -0,0 +1,13 @@
+package service.Impl;
+
+import model.Teacher;
+import service.ITeacherBService;
+
+import java.util.List;
+
+public class TeacherBSerivce implements ITeacherBService {
+    @Override
+    public List<Teacher> getAll() {
+        return null;
+    }
+}
